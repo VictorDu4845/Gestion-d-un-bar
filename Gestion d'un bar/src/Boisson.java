b417a0913ee7281db443a342469536b65ba19534
Victor <Victor@192.168.0.48>
2018-11-30 18:17:52 +0100

    "Commit" du 30 novembre

--- Gestion d'un bar/src/Boisson.java
+++ Gestion d'un bar/src/Boisson.java
@@ -1,4 +1,26 @@
-
 public class Boisson {
-
+	public String nom;
+	public double prix;
+	public boolean estSoft;
+	
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	
+	public void setEstSoft(boolean estSoft) {
+		this.estSoft = estSoft;
+	}
+	public boolean getEstSoft() {
+		return estSoft;
+	}
 }
