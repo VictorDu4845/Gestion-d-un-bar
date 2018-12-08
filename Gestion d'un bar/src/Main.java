c8b87b27b200f20409496608b89111bac74ecbac
Victor <Victor@192.168.0.48>
2018-12-08 03:56:11 +0100

    Commit du 8 décembre

--- Gestion d'un bar/src/Main.java
+++ Gestion d'un bar/src/Main.java
@@ -4,15 +4,13 @@ public class Main {
 		BoissonEtendue coca = new BoissonEtendue ("Coca", 1, 0.00, 1);
 		BoissonEtendue sprite = new BoissonEtendue ("Sprite", 1, 0.00, 1);
 		BoissonEtendue binouze = new BoissonEtendue ("Bière", 2, 0.00, 10);
-		System.out.println("On passe maintenant aux tickets de caisse");
-		TicketCaisse x = new TicketCaisse();
-		x.ajouter(coca);
-		x.ajouter(binouze);
-		x.ajouter(sprite);
-		x.afficherTicket();
-		x.enleverDernierEncodage();
-		x.afficherTicket();
-		
-		;
+		System.out.println("On passe maintenant aux tickets de caisse" + "\n");
+		TicketCaisse victor = new TicketCaisse();
+		victor.ajouter(coca);
+		victor.ajouter(binouze);
+		victor.ajouter(sprite);
+		victor.afficherTicket();
+		victor.enleverDernierEncodage();
+		victor.afficherTicket();
 	}
 }
